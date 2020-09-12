7ab22efb4580e0a122365b462653967f9b1c90a1
Bikal Thapa <Bik182>
2020-09-12 18:14:47 -0500

    v2

---
+++ PROJECT/p1/src/MazeSolver.java
@@ -0,0 +1,14 @@
+package PROJECT.p1.src;
+
+import java.util.Scanner;
+
+public class MazeSolver{
+    public static void main(String[] args){
+
+    }
+
+    public MazeSolver(Scanner s){
+
+    }
+}
+
